cbc04f0b16f59552cb0a7c435f958f6e4ba9374b
sumnimanepal <49048755+sumnimanepal@users.noreply.github.com>
2019-07-01 14:01:14 +0545

    Add files via upload

---
+++ verify.js
@@ -0,0 +1,9 @@
+exports.user = (req, res, next) => {
+    if (!req.user) {
+        let err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
+    } else {
+        next();
+    }
+};
